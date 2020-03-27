d4c9edf4e6a74c1a63ef9ac30a6b9319912ec0ab
gota morishita <optimistic.gota@gmail.com>
2020-03-27 15:23:36 +0900

    update

--- ch2/recommendations.py
+++ ch2/recommendations.py
@@ -1,4 +1,5 @@
 from math import sqrt
+from collections import defaultdict
 import numpy as np
 
 # A dictionary of movie critics and their ratings of a small set of movies
@@ -89,7 +90,6 @@ def calc_similarity_pearson(prefs, p1, p2):
     p2_shared_ratings = np.array([prefs[p2][item] for item in shared_items])
     
     # Strictly spaeking, average ratings should be calculated with shared items only, but we calculate average ratings with all of the items each person rated
-    import ipdb; ipdb.set_trace()
     p1_ave_rating = np.fromiter(prefs[p1].values(), dtype=np.float32).mean()
     p2_ave_rating = np.fromiter(prefs[p2].values(), dtype=np.float32).mean()
 
@@ -101,3 +101,61 @@ def calc_similarity_pearson(prefs, p1, p2):
     corr = ((p1_shared_ratings - p1_ave_rating) * (p2_shared_ratings - p2_ave_rating)).sum()
 
     return corr / ( p1_std_rate * p2_std_rate )
+
+def top_matches(prefs, person, n=5, calc_similarity=calc_similarity_pearson):
+    '''
+    Return the best matches for person from the prefs dictionary
+    Number of results and similarity function are optional params
+    '''
+    scores = [(calc_similarity(prefs, person, other), other) for other in prefs.keys() if other != person]
+
+    # sort the list so the highest scors appear at the top
+    scores.sort(key = lambda x : x[0], reverse=True)
+    return scores[:n]
+
+
+def get_recommendations(prefs, person, calc_similarity=calc_similarity_pearson):
+    '''
+    Gets recommendations for a person by using a weighted average of every other users' rankings
+    '''
+    weighted_rating_list = defaultdict(float)
+    total_similarity = defaultdict(float)
+
+    for other in prefs:
+        # don't compare me to myself
+        if other == person:
+            continue
+
+        # ignores zero or negative similarity
+        similarity = calc_similarity(prefs, person, other)
+        if similarity <= 0:
+            continue
+
+        for item in prefs[other]:
+            # ignores items that I haven't seen yet
+            if item not in prefs[person] or prefs[person][item] == 0:
+               weighted_rating_list[item] += prefs[other][item] * similarity
+               total_similarity[item] += similarity
+
+    rankings = [ (total_score / total_similarity[item], item) for item, total_score in weighted_rating_list.items() ]
+    rankings.sort(reverse=True)
+    return rankings
+
+
+def transformPrefs(prefs):
+    '''
+    transform user-based preference dictionary to item-based preference dictionary
+    '''
+    results = defaultdict(dict)
+    for person in prefs.keys():
+        for item in prefs[person]:
+            # flip item and person
+            results[item][person] = prefs[person][item]
+
+    return results
+
+from pprint import pprint
+movies = transformPrefs(critics)
+pprint(movies)
+pprint(top_matches(movies, 'Superman Returns'))
+pprint(get_recommendations(movies, 'Just My Luck'))
